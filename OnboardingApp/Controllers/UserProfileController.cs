6035f21de5818be64b67111b60b43c11fe0d8e16
froska <dafroska@yahoo.com>
2019-08-28 02:07:58 +0100

    Added Edit User Service

--- OnboardingApp/Controllers/UserProfileController.cs
+++ OnboardingApp/Controllers/UserProfileController.cs
@@ -27,13 +27,125 @@ namespace OnboardingApp.Controllers
         {
             string userId = User.Claims.First(c => c.Type == "UserID").Value;
             var user = await _userManager.FindByIdAsync(userId);
-            return new
-            {
+            return new{
                 user.FullName,
                 user.Email,
                 user.UserName
             };
         }
+
+        [HttpGet]
+        [Route("ListUsers")]
+        public IActionResult GetListUsers()
+        {
+            List<ApplicationUserModel> users = new List<ApplicationUserModel>();
+            foreach (var user in _userManager.Users)
+            {
+                users.Add(new ApplicationUserModel{
+                    FullName=user.FullName,
+                    Email=user.Email,
+                    UserName=user.UserName
+                });
+
+            }
+            return Ok(users);
+        }
+
+        [HttpGet]
+        [Route("GetUserByID/{id}")]
+        public async Task<IActionResult> GetUserByID(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user != null)
+            {
+                return Ok(new{
+                    user.FullName,
+                    user.Email,
+                    user.UserName
+                });
+            }
+
+            return NotFound();
+        }
+
+        [HttpGet]
+        [Route("GetUserByName/{username}")]
+        public async Task<IActionResult> GetUserByName(string username)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user != null)
+            {
+                return Ok(new
+                {
+                    user.FullName,
+                    user.Email,
+                    user.UserName
+                });
+            }
+
+            return NotFound();
+
+        }
+
+
+        [HttpGet]
+        [Route("EditUser/{id}")]
+        public async Task<IActionResult> UpdateUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            ApplicationUserModel updatedUser;
+
+            if (user != null)
+            {
+               updatedUser =  new ApplicationUserModel
+                {
+                    ID = user.Id,
+                    FullName = user.FullName,
+                    Email = user.Email,
+                    UserName = user.UserName
+                };
+
+                return Ok(updatedUser);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        [Route("EditUser")]
+        public async Task<IActionResult> UpdateUser(ApplicationUserModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.ID);
+           
+            if (user != null)
+            {
+                user.FullName = model.FullName;
+                user.Email = model.Email;
+                user.UserName = model.UserName;
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return Ok(user);
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+
+                    return BadRequest(ModelState); 
+                    
+
+                } 
+                
+            }
+
+            return NotFound();
+        }
+
 
         [HttpGet]
         [Authorize(Roles = "Admin")]
